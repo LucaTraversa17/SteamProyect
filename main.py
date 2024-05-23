9aacfafd1b397f03e7d388f0c280c015b26c8b8d
Luca Traversa <88990751+LucaTraversa17@users.noreply.github.com>
2024-05-23 14:54:31 -0300

    Update main.py
    Cambiamos el puerto para que pueda andar en render

--- main.py
+++ main.py
@@ -140,4 +140,4 @@ def read_root():
     return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
